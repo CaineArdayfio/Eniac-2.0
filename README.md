fa4c21f7807449f109bd13c1e5a4cec1d1349870
Caine Ardayfio <ardayfiocaine@gmail.com>
2021-07-06 01:27:16 -0400

    This is the full Eniac Professional debut!!!

--- README.md
+++
@@ -1,7 +0,0 @@
-This was at first a copy of django's poll voting app (I followed this tutorial to make it https://docs.djangoproject.com/en/3.2/intro/tutorial01/) that I then deployed to azure.
-Here are all the tutorials i the series
-https://docs.djangoproject.com/en/3.2/intro/tutorial02/
-https://docs.djangoproject.com/en/3.2/intro/tutorial03/
-https://docs.djangoproject.com/en/3.2/intro/tutorial04/
-https://docs.djangoproject.com/en/3.2/intro/tutorial05/
-https://docs.djangoproject.com/en/3.2/intro/tutorial06/
---
+++ app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class User(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+    phone_number = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=200, primary_key=True)
+    users = models.ManyToManyField(User)
+    def __str__(self):
+        return self.group_name
+
+class Question(models.Model):
+    question_name = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200)
+    time = models.DateTimeField()
+    groups = models.ManyToManyField(Group)
+
+    class Meta:
+        unique_together = ('question_name','question_text', 'time',)
+
+    def __str__(self):
+        return self.question_name
